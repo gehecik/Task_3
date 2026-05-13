d226ce45dfbd59269eb7b1629149709fb6f897d9
Kechek Karine <kechek.karine@gmail.com>
2026-05-13 15:40:23 +0300

    add HomePage

--- src/test/java/org/example/pages/user/HomePage.java
+++ src/test/java/org/example/pages/user/HomePage.java
@@ -1,13 +1,16 @@
-package org.example;
+package org.example.pages.user;
 
+import io.qameta.allure.Step;
+import org.example.pages.BasePage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
+
 import static org.example.utils.EnvConfig.BASE_URL;
 
-public class HomePage extends BasePage{
-    protected final By accountLink = By.xpath("//a[contains(@href,'/account')]");
-
+public class HomePage extends BasePage {
+    protected final By accountLink = By.cssSelector("a[href='/account']");
+    protected final By loginToAccount = By.xpath("//button[text()='Войти в аккаунт']");
 
     public HomePage(WebDriver driver) {
         super(driver);
@@ -16,11 +19,39 @@ public class HomePage extends BasePage{
     public void openPage() {
         driver.get(BASE_URL);
     }
+    public void openPageAndWait() {
+        openPage();
+        waitLocator(accountLink);
+    }
 
+
+    @Step("Click on the link (accountLink)")
     public LoginPage clickAccountLink() {
+        waitLocator(accountLink);
         waitClickable(accountLink).click();
 
         return new LoginPage(driver);
+    }
+
+    @Step("Click on the button (loginToAccount)")
+    public LoginPage clickLoginToAccount() {
+        waitLocator(loginToAccount);
+        waitClickable(loginToAccount).click();
+
+        return new LoginPage(driver);
+    }
+
+    @Step("Check")
+    public void checkHomePage() {
+        checkLocator(loginToAccount);
+    }
+
+    @Step("Click on the link (accountLinkUser)")
+    public ProfilePage clickAccountLinkUser() {
+        waitLocator(accountLink);
+        waitClickable(accountLink).click();
+
+        return new ProfilePage(driver);
     }
 
 }
