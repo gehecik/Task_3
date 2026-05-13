a6fef8207b26b459cf0161cb5f3f4fc7218554ba
Kechek Karine <kechek.karine@gmail.com>
2026-05-13 15:41:30 +0300

    add HomeAuthPage

--- src/test/java/org/example/pages/user/HomeAuthPage.java
+++ src/test/java/org/example/pages/user/HomeAuthPage.java
@@ -1,17 +1,77 @@
-package org.example;
+package org.example.pages.user;
 
+import io.qameta.allure.Step;
+import org.example.pages.BasePage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
-public class HomeAuthPage extends BasePage{
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class HomeAuthPage extends BasePage {
 
     protected final By locatorAccessToken = By.xpath("//p[text()='Личный Кабинет']");
+    protected final By userStatusContent = By.className("button_button_size_large__G21Vg");
+    protected final By accountLink = By.cssSelector("a[href='/account']");
+    protected final By tabCurrent = By.cssSelector("div[class*='current']");
+    protected final  By profileLink = By.xpath("//a[contains(@href,'/account/profile')]");
 
     public HomeAuthPage(WebDriver driver) {
         super(driver);
     }
 
+    @Step("Get accessToken")
     public String getToken() throws InterruptedException {
         return getAccessToken(locatorAccessToken);
     }
+
+    @Step("Check")
+    public void checkStatusContentLoginAccount() {
+        checkLocator(userStatusContent);
+    }
+    @Step("Click on the link")
+    public ProfilePage clickAccountLinkRegisterUser() {
+        waitClickable(accountLink).click();
+
+        waitLocator(profileLink);
+
+        return new ProfilePage(driver);
+    }
+
+    @Step("Get current tab")
+    public WebElement getCurrentTab() {
+        checkLocator(tabCurrent);
+        return waitClickable(tabCurrent);
+    }
+
+    @Step("Check current tab (tabCurrent)")
+    public void checkCurrentTab(String tabName) throws InterruptedException {
+        By tabTitle = By.xpath("//h2[contains(@class,'text_type_main-medium') and text()='" + tabName + "']");
+        waitClickable(tabCurrent).click();
+        checkLocator(tabTitle);
+
+        WebElement element = waitClickable(tabCurrent);
+
+        String value = element.getAttribute("class");
+        assertTrue(value.contains("current"));
+
+        String actualValue = element.findElement(By.tagName("span")).getText();
+        assertEquals(tabName, actualValue);
+    }
+
+
+    public void clickCurrentTab(String tabName) {
+        By tabXpath = By.xpath("//div[contains(@class,'tab')][.//span[text()='" + tabName + "']]");
+        checkLocator(tabXpath);
+
+        WebElement element = waitClickable(tabXpath);
+
+        String classValue = element.getAttribute("class");
+
+        if (!classValue.contains("tab_tab_type_current")) {
+            element.click();
+        }
+    }
+
 }
