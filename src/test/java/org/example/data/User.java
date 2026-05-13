f01230b4191ffabc0b8aa5f1d4672b06ea2c001e
Kechek Karine <kechek.karine@gmail.com>
2026-05-13 15:38:18 +0300

    add user

--- src/test/java/org/example/data/User.java
+++ src/test/java/org/example/data/User.java
@@ -1,4 +1,35 @@
 package org.example.data;
 
+import lombok.*;
+
+import static org.example.utils.RandomValue.randomEmail;
+import static org.example.utils.RandomValue.randomNumberAsString;
+
+@Builder
+@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
+    private String email;
+    private String password;
+    private String name;
+
+    public static User userWithRandomField() {
+        return User.builder()
+                .name("name")
+                .email(randomEmail())
+                .password(randomNumberAsString())
+                .build();
+    }
+
+    public static User userWithWrongPassword() {
+        return User.builder()
+                .name("name")
+                .email(randomEmail())
+                .password("123")
+                .build();
+    }
+
 }
